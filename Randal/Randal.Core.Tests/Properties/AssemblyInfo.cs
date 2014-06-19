97212e25dbd095e986f57b615b7c73cf596bf46f
nick-randal <nick@randalfamily.com>
2014-06-18 22:21:16 -0400

    Adding GNU

--- Randal/Randal.Core.Tests/Properties/AssemblyInfo.cs
+++ Randal/Randal.Core.Tests/Properties/AssemblyInfo.cs
@@ -1,14 +1,29 @@
-﻿using System.Reflection;
+﻿/*
+Useful C#
+Copyright (C) 2014  Nicholas Randal
+
+Useful C# is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Randal.Core.Tests")]
+[assembly: AssemblyTitle("Randal.Tests.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicholas Randal")]
-[assembly: AssemblyProduct("Randal.Core.Tests")]
+[assembly: AssemblyProduct("Randal.Tests.Core")]
 [assembly: AssemblyCopyright("Copyright © 2014 Nicholas Randal, All Rights Reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
