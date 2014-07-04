c33222aeaafe237acc542a58a80c60b0d23b07a8
nick-randal <nick@randalfamily.com>
2014-07-04 14:57:01 -0400

    Added OwinMiddleware logger support

---
+++ Randal/Randal.Logging/Owin/LoggerOwinFormatter.cs
@@ -0,0 +1,48 @@
+﻿// Useful C#
+// Copyright (C) 2014 Nicholas Randal
+// 
+// Useful C# is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+using Microsoft.Owin;
+
+namespace Randal.Logging.Owin
+{
+	public interface ILoggerOwinFormatter
+	{
+		bool UsePreEntry { get; }
+		ILogEntry GetPreEntry(IOwinContext context);
+		bool UsePostEntry { get; }
+		ILogEntry GetPostEntry(IOwinContext context);
+	}
+
+	public sealed class LoggerOwinFormatter : ILoggerOwinFormatter
+	{
+		public bool UsePreEntry
+		{
+			get { return true; }
+		}
+
+		public ILogEntry GetPreEntry(IOwinContext context)
+		{
+			return new LogEntry(" in  " + context.Environment["owin.RequestPath"]);
+		}
+
+		public bool UsePostEntry
+		{
+			get { return true; }
+		}
+
+		public ILogEntry GetPostEntry(IOwinContext context)
+		{
+			return new LogEntry("out  " + context.Environment["owin.RequestPath"]);
+		}
+	}
+}
---
+++ Randal/Randal.Logging/Owin/LoggerOwinMiddleware.cs
@@ -0,0 +1,52 @@
+﻿// Useful C#
+// Copyright (C) 2014 Nicholas Randal
+// 
+// Useful C# is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Randal.Logging.Owin
+{
+	public sealed class LoggerOwinMiddleware : OwinMiddleware, IDisposable
+	{
+		public LoggerOwinMiddleware(OwinMiddleware next, ILogger logger) : this(next, logger, null)
+		{
+		}
+
+		public LoggerOwinMiddleware(OwinMiddleware next, ILogger logger, ILoggerOwinFormatter formatter)
+			: base(next)
+		{
+			_logger = logger;
+			_formatter = formatter ?? new LoggerOwinFormatter();
+		}
+
+		public override async Task Invoke(IOwinContext context)
+		{
+			if(_formatter.UsePreEntry)
+				_logger.Add(_formatter.GetPreEntry(context));
+
+			await Next.Invoke(context);
+
+			if(_formatter.UsePostEntry)
+				_logger.Add(_formatter.GetPostEntry(context));
+		}
+
+		public void Dispose()
+		{
+			_logger.Dispose();
+		}
+
+		private readonly ILogger _logger;
+		private readonly ILoggerOwinFormatter _formatter;
+	}
+}
