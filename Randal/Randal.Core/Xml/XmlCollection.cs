256de2db745969e5137ddfb31d1bef8822b1b07b
nick-randal <nick@randalfamily.com>
2014-07-05 09:17:20 -0400

    Added XmlCollection to core

---
+++ Randal/Randal.Core/Xml/XmlCollection.cs
@@ -0,0 +1,37 @@
+﻿// Useful C#
+// Copyright (C) 2014 Nicholas Randal
+// 
+// Useful C# is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+using System.Collections.Generic;
+
+namespace Randal.Core.Xml
+{
+	public sealed class XmlCollection<TItem>
+	{
+		public List<TItem> Items { get; private set; }
+
+		public XmlCollection()
+			: this(null)
+		{
+		}
+
+		public XmlCollection(IEnumerable<TItem> items)
+		{
+			Items = items == null ? new List<TItem>() : new List<TItem>(items);
+		}
+
+		public void Add(object o)
+		{
+			Items.Add((TItem)o);
+		}
+	}
+}
