af51944e400fa767c387b1cbbb4aad9f61b80248
nick-randal <nick@randalfamily.com>
2014-09-06 12:01:05 -0400

    Added new project for Quick Xml

---
+++ Randal/Randal.QuickXml/Randal.QuickXml/QuickXmlParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randal.QuickXml
+{
+	public interface IQuickXmlParser
+	{
+
+	}
+
+	public class QuickXmlParser : IQuickXmlParser
+	{
+	}
+}
---
+++ Randal/Randal.QuickXml/Randal.Tests.QuickXml/QuickXmlParserTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Randal.Core.Testing.UnitTest;
+using FluentAssertions;
+using Randal.QuickXml;
+using System.Xml.Linq;
+
+namespace Randal.Tests.QuickXml
+{
+	[TestClass]
+	public sealed class QuickXmlParserTests : BaseUnitTest<QuickXmlParserThens>
+	{
+		[TestMethod, PositiveTest]
+		public void ShouldHaveValidInstance_WhenCreating_GivenDefaults()
+		{
+			When(Creating);
+
+			Then.Target.Should().NotBeNull().And.Should().BeAssignableTo<IQuickXmlParser>();
+		}
+
+		[TestMethod, PositiveTest]
+		public void ShouldHaveXmlDocument_WhenParsing_GivenDocumentStructure()
+		{
+			Given.Text = "?1.0\nA\n\tB";
+
+			When(Parsing);
+
+			Then.Document.Should().NotBeNull();
+		}
+
+		private void Parsing()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void Creating()
+		{
+			Then.Target = new QuickXmlParser();
+		}
+	}
+
+	public sealed class QuickXmlParserThens
+	{
+		public QuickXmlParser Target;
+		public XDocument Document;
+
+	}
+}
