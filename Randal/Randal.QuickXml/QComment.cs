6ca1bdf0ccc55ef187659c5ad73536892d0d8383
nick-randal <nick@randalfamily.com>
2014-09-10 19:57:52 -0400

    File missed from last commit

---
+++ Randal/Randal.QuickXml/QComment.cs
@@ -0,0 +1,32 @@
+﻿// // Useful C#
+// // Copyright (C) 2014 Nicholas Randal
+// // 
+// // Useful C# is free software; you can redistribute it and/or modify
+// // it under the terms of the GNU General Public License as published by
+// // the Free Software Foundation; either version 2 of the License, or
+// // (at your option) any later version.
+// // 
+// // This program is distributed in the hope that it will be useful,
+// // but WITHOUT ANY WARRANTY; without even the implied warranty of
+// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// // GNU General Public License for more details.
+
+using System;
+using System.Xml;
+
+namespace Randal.QuickXml
+{
+	public sealed class QComment : QuickXmlItem
+	{
+		public QComment(int depth, string value)
+			: base(XmlNodeType.Comment, depth)
+		{
+			Value = value;
+		}
+
+		public override string Name
+		{
+			get { throw new NotSupportedException(); }
+		}
+	}
+}
