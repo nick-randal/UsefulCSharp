a5a1ef0e1c72c774f8933ee2dc912ce2d1c10cd9
Nick Randal <nick@randalfamily.com>
2015-12-16 20:21:59 -0500

    Changed core config.json

---
+++ Randal/Randal.Sql.Scripting.App/Scripts/Core/Sprocs/coreCreateSchema.sql
@@ -0,0 +1,38 @@
+--:: need coreTrimBrackets
+--:: catalog %
+
+--:: ignore
+use model
+
+--:: pre
+exec coreCreateProcedure 'coreCreateSchema'
+GO
+
+--:: main
+alter procedure coreCreateSchema
+	@schemaName		sysname = 'dbo',
+	@debug			bit = 0
+as
+	DECLARE @cmd varchar(max)
+
+	IF EXISTS (SELECT 1 FROM sys.schemas WHERE name = @schemaName) begin
+		return;
+	end
+		
+	set @schemaName = dbo.coreTrimBrackets(@schemaName)
+	SET @cmd = 'CREATE SCHEMA [' + @schemaName + ']'
+	
+	if(@debug = 1) begin
+		print @cmd
+	end else begin
+		exec(@cmd)
+	end
+GO
+
+/*
+
+exec coreCreateSchema 'Test', 1
+
+select * from sys.schemas
+
+*/
