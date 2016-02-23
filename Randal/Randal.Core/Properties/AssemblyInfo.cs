4d113ac4bb1adf7a1c8d54c55f2ed8327d990d2f
Nick Randal <nick@randalfamily.com>
2016-02-23 12:57:08 -0500

    Updated license info

--- Randal/Randal.Core/Properties/AssemblyInfo.cs
+++ Randal/Randal.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// Useful C#
-// Copyright (C) 2014-2015 Nicholas Randal
+// Copyright (C) 2014-2016 Nicholas Randal
 // 
 // Useful C# is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicholas Randal")]
 [assembly: AssemblyProduct("Randal.Core")]
-[assembly: AssemblyCopyright("Copyright © 2014-2015 Nicholas Randal, All Rights Reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 Nicholas Randal, All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
