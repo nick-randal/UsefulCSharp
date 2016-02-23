13752c82f55ab2f13a6d7a06802c1751d59169ef
Nick Randal <nick@randalfamily.com>
2016-02-23 13:06:34 -0500

    Added missing files

---
+++ Randal/Randal.Logging/ILogSink.cs
@@ -0,0 +1,23 @@
+﻿// Useful C#
+// Copyright (C) 2014-2016 Nicholas Randal
+// 
+// Useful C# is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+using System;
+
+namespace Randal.Logging
+{
+	public interface ILogSink : IDisposable
+	{
+		Verbosity VerbosityThreshold { get; }
+		void Post(ILogEntry entry);
+	}
+}
---
+++ Randal/Randal.Logging/NullLogger.cs
@@ -0,0 +1,77 @@
+﻿// Useful C#
+// Copyright (C) 2014-2016 Nicholas Randal
+// 
+// Useful C# is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Randal.Logging
+{
+	public sealed class NullLogger : ILogger
+	{
+		public void AddLogSink(ILogSink logSink) { }
+
+		public Task CompleteAllAsync(int attemptsToComplete = 3, TimeSpan? delayBetweenAttempts = null)
+		{
+			return Task.FromResult(false);
+		}
+
+		public Task PostEntryAsync(string message, params object[] values)
+		{
+			return Task.FromResult(false);
+		}
+
+		public void PostEntry(string message, params object[] values) { }
+
+		public Task PostEntryAsync(Verbosity verbosity, string message, params object[] values)
+		{
+			return Task.FromResult(false);
+		}
+
+		public void PostEntry(Verbosity verbosity, string message, params object[] values) { }
+
+		public Task PostBlankAsync(Verbosity verbosity = Verbosity.Info)
+		{
+			return Task.FromResult(false);
+		}
+
+		public void PostBlank(Verbosity verbosity = Verbosity.Info) { }
+
+		public Task PostEntryNoTimestampAsync(string message, params object[] values)
+		{
+			return Task.FromResult(false);
+		}
+
+		public void PostEntryNoTimestamp(string message, params object[] values) { }
+
+		public Task PostEntryNoTimestampAsync(Verbosity verbosity, string message, params object[] values)
+		{
+			return Task.FromResult(false);
+		}
+
+		public void PostEntryNoTimestamp(Verbosity verbosity, string message, params object[] values) { }
+
+		public Task PostExceptionAsync(Exception ex)
+		{
+			return Task.FromResult(false);
+		}
+
+		public void PostException(Exception ex) { }
+
+		public Task PostExceptionAsync(Exception ex, string message, params object[] values)
+		{
+			return Task.FromResult(false);
+		}
+
+		public void PostException(Exception ex, string message, params object[] values) { }
+	}
+}
