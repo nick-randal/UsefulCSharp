00de66c21b9fe053ef2b94c0589f0a910f20c87f
Nick Randal <nrandal@trialcard.com>
2017-02-01 10:29:17 -0500

    Changed projects to .net 4.6.2

--- Randal/Randal.Core.Testing.Factory/Properties/AssemblyInfo.cs
+++ Randal/Randal.Core.Testing.Factory/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicholas Randal")]
 [assembly: AssemblyProduct("Randal.Core.Testing.Factory")]
-[assembly: AssemblyCopyright("Copyright © 2014-2016 Nicholas Randal, All Rights Reserved")]
+[assembly: AssemblyCopyright("Copyright © 2014-2017 Nicholas Randal, All Rights Reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
